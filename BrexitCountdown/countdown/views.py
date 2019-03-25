d2ce1fb57580af96e234980bce013e73fc1824d6
Aron Van der Haegen <atomicprobomb@gmail.com>
2019-03-25 16:20:12 +0100

    Final update I guess

--- BrexitCountdown/countdown/views.py
+++ BrexitCountdown/countdown/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 import datetime
 def hello(request):
   
-   brexit = datetime(2019, 3, 30, 00, 00, 00)
-   result = (brexit - datetime.datetim.now())
-   text = """<h1>Time until Brexit: """ + result + """ </h1>"""
+   #brexit = datetime(2019, 3, 30, 00, 00, 00)
+   #result = (brexit - datetime.datetime.now())
+   text = """<h1>Time until Brexit: </h1>"""
    return HttpResponse(text)
