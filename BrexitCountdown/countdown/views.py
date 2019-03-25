f27a3d6f34df8d627379a37166d8a9a7bbccb533
Aron Van der Haegen <atomicprobomb@gmail.com>
2019-03-25 15:05:18 +0100

    DateCalculation
    Added calculations for the datetime difference.

--- BrexitCountdown/countdown/views.py
+++ BrexitCountdown/countdown/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+import datetime
+def hello(request):
+  
+   brexit = datetime(2019, 3, 30, 00, 00, 00)
+   result = (brexit - datetime.datetim.now())
+   text = """<h1>Time until Brexit: """ + result + """ </h1>"""
+   return HttpResponse(text)
